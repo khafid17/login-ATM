df170c4ece4b689eb958a6bdb2ecc20cfd759a74
khafid17 <khafiddurrahman@gmail.com>
2020-12-28 19:21:49 +0700

    login ATM

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int pilihan=12345,n,j,t;
+    string tarik;
+
+    cout << "=====SELAMAT DATANG DI LAYANAN ATM=====\n";
+    cout << "MASUKAN PIN = ";
+    cin >>pilihan;
+    {
+        if(pilihan==12345)
+        {
+        cout<< "SELAMAT DATANG\n";
+        cout << "======MENU UTAMA======\n";
+        cout<< "\n1. TRANSAKSI \n2. INFORMASI \n3. SELESI\n";
+        cout << "===================================================================\n";
+        cout << "Pilihan Anda : ";
+        cin >>pilihan;
+        else if{
+            cout<< "===================================================================\n";
+            cout<< "MAAF PIN ANDA SALAH"
+            goto menu;
+        }
+        if(pilihan == 1){
+
+        cout << "======TRANSAKSI REKEING BNI======\n";
+        cout<< "Nomor Rekening Tujuan : ";
+        cin>>n;
+        cout<< "Jumlah Transaksi : ";
+        cin>>j;
+        cout<< "Tanggal Transaksi : ";
+        cin>>t;
+
+}}}        }
+
+
+
+
